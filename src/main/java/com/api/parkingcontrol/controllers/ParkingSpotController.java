1b4c209071d6f2162634100946dbcf8494f48e3a
Wagner <wagneroliveirap@gmail.com>
2022-03-09 12:03:29 -0300

    Metodo GET

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -2,13 +2,19 @@ package com.api.parkingcontrol.controllers;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,12 +45,27 @@ public class ParkingSpotController {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use!");
 		}
 		if (parkingSpotService.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered for this apartment/block!");
+			return ResponseEntity.status(HttpStatus.CONFLICT)
+					.body("Conflict: Parking Spot already registered for this apartment/block!");
 		}
 		var parkingSpotModel = new ParkingSpotModel();
 		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
 		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
 		return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+	}
+
+	@GetMapping
+	    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(){
+	        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll();
+	    }
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id) {
+		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+		if (!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
 	}
 
 }
