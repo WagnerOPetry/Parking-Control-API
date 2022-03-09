f388f277f72a4379ea2f2b3bb27f490aec8665b1
Wagner <wagneroliveirap@gmail.com>
2022-03-09 12:11:58 -0300

    Metodo Delete e PUT

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -45,6 +45,11 @@ public class ParkingSpotService {
 		return parkingSpotRepository.findById(id);
 	}
 
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+	}
+
 	 
 
 }
